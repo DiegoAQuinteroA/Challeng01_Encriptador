9984d57cf58e937617dce6a42a09410535df5c86
Diego Quintero <quinterodiego20@hotmail.com>
2023-05-01 10:05:08 -0500

    scrip.js funcionando y estructura html

---
+++ script.js
@@ -0,0 +1,55 @@
+//DOM
+let btnEncriptar = document.getElementById("encriptar");
+let btnDesencriptar = document.getElementById("desencriptar");
+let infoOculto = document.getElementById("oculto");
+let btnCopiar = document.getElementById("copiar");
+let textSalida = document.querySelector(".texto-salida");
+let imgSalida = document.getElementsByClassName(".img-salida");
+
+function encriptar (){
+    let texto = document.querySelector("#entrada").value;
+    let textoCifrado = texto.replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+
+    document.querySelector(".texto-salida").value = textoCifrado;
+    document.querySelector("#entrada").value;
+
+    infoOculto.style.display = 'none'; //ocultar la información del recuadro de salida
+    btnCopiar.style.display = 'block'; //mostrar boton copiar
+    textSalida.style.display = 'block';
+    imgSalida.style.display = 'none'
+}
+btnEncriptar.onclick = encriptar;
+
+function desencriptar (){ 
+    let texto = document.querySelector("#entrada").value; 
+    let textoCifrado = texto.replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u"); 
+    document.querySelector(".texto-salida").value = textoCifrado;
+    document.querySelector("#entrada").value;
+
+    infoOculto.style.display = 'none'; //ocultar la información del recuadro de salida
+    btnCopiar.style.display = 'block'; //mostrar boton copiar
+    textSalida.style.display = 'block';
+    imgSalida.style.display = 'none'
+}
+btnDesencriptar.onclick = desencriptar;
+
+//copiar
+btnCopiar.addEventListener("click", (event) => {
+    event.preventDefault();
+    textSalida.select();
+    document.execCommand("copy");
+});
+
+// ocultar boton copiar 
+btnCopiar.style.display = 'none';
+textSalida.style.display = 'none';
+
+
